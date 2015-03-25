a2fa274c21d5035f8b25cc61d7214020f1705e74
David Hayes <david@blackbricksoftware.com>
2015-03-25 15:33:55 -0500

    remove debug code

--- CRM/Core/IDS.php
+++ CRM/Core/IDS.php
@@ -1,9 +1,9 @@
 <?php
 /*
  +--------------------------------------------------------------------+
- | CiviCRM version 4.5                                                |
+ | CiviCRM version 4.4                                                |
  +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2014                                |
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
  +--------------------------------------------------------------------+
  | This file is a part of CiviCRM.                                    |
  |                                                                    |
@@ -28,7 +28,7 @@
 /**
  *
  * @package CRM
- * @copyright CiviCRM LLC (c) 2004-2014
+ * @copyright CiviCRM LLC (c) 2004-2013
  * $Id$
  *
  */
@@ -208,10 +208,7 @@ class CRM_Core_IDS {
    * This function writes an entry about the intrusion
    * to the intrusion database
    *
-   * @param $result
-   * @param int $reaction
-   *
-   * @internal param array $results
+   * @param array $results
    *
    * @return boolean
    */
